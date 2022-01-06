1acd8dd7061d2d5ceecb2fcdcb05ba07a491421d
EdPeReg <dardo1818@gmail.com>
2022-01-05 22:19:22 -0600

    Algorithm from wikipedia used, time limit exceeded

--- Longest Palindromic Substring/main.cpp
+++ Longest Palindromic Substring/main.cpp
@@ -1,40 +1,49 @@
+// TIME LIMITE EXCEEDED.
+// Slow algorithm from wikipedia used.
+
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-bool is_palindrome(string& substr)
-{
-    int right = substr.size() - 1;
-    for(size_t i = 0; i < substr.size() / 2; ++i) {
-        if(substr[i] != substr[right]) return false;
-        --right;
-    }
-    return true;
-}
-
 string longestPalindrome(string& s) {
     string substr;
     vector<string> palindromes(2,"");
-    size_t left = 0, i = 0;
 
-    while(i < s.size())
+    for(size_t i = 0; i <= s.size(); i += 2)
+        s.insert(i, "|");
+
+    int current_pos = 1;
+    while(current_pos < s.size())
     {
-        substr += s[i];
-        if(is_palindrome(substr))
+        int left = current_pos - 1;
+        int right = current_pos + 1;
+
+        // Iterate thorugh the left and right, obtaining a palindrome.
+        while(left > -1 and right < s.size())
         {
-            palindromes[0] = substr;
-            if(palindromes[0].size() > palindromes[1].size())
-                palindromes[1] = palindromes[0];
+            if(s[left] == s[right])
+                // The substraction is done to get the correct position.
+                substr = s.substr(left, right - left + 1);
+            // If both chars don't match already, just brake, no need to continue checking.
+            else break;
+
+            --left;
+            ++right;
         }
 
-        if(++i == s.size()) {
-            palindromes[0] = "";
-            substr = "";
-            i = ++left;
-        }
+        // There could be multiple palindromes in the string, just get the longest one.
+        palindromes[0] = substr;
+        if(palindromes[0].size() > palindromes[1].size())
+            palindromes[1] = palindromes[0];
+        ++current_pos;
     }
 
+    // std::erase(palindromes[1], '|');
+    size_t pos = 0;
+    while((pos = palindromes[1].find("|")) != std::string::npos)
+        palindromes[1].erase(pos, 1);
     return palindromes[1];
 }
 
