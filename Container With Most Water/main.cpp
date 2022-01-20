7331ec9c43adba86e568ecf3dc42bddcb140e8e8
EdPeReg <dardo1818@gmail.com>
2022-01-19 22:34:54 -0600

    O(n^2) time limit exceeded, refactored

--- Container With Most Water/main.cpp
+++ Container With Most Water/main.cpp
@@ -2,36 +2,24 @@
 #include <vector>
 
 int maxArea(std::vector<int>& height) {
-    int maxFirstPos = 0, maxLastPos = 0;
-    int maxArea1 = 0, maxArea2 = 0;
+    int currentArea = 0, result = 0;
 
     for(size_t i = 0; i < height.size(); ++i) {
-        bool first = false;
-
-        for(size_t j = 0; j < height.size(); ++j) {
-            if(first == false and height[j] >= height[i]) {
-                maxFirstPos = j; 
-                first = true;
-            }
-
-            if(height[j] >= height[i]) {
-                maxLastPos = j; 
-            }
+        for(size_t j = i+1; j < height.size(); ++j) {
+            size_t distance = j-i;
+            currentArea = std::min(height[i], height[j]) * distance;
+            result = std::max(result, currentArea);
         }
-
-        maxArea2 = (&height[maxLastPos] - &height[maxFirstPos]) * height[i];
-        if(maxArea2 > maxArea1)
-            maxArea1 = maxArea2;
     }
 
-    return maxArea1;
+    return result;
 }
 
 int main() {
     int n;
     std::cin >> n;
     std::vector<int> v(n);
-    
+
     for(auto &e : v)
         std::cin >> e;
 
