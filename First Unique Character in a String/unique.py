840ca68d1d729370b871cdb11c1aefcc5800d732
EdPeReg <dardo1818@gmali.com>
2023-04-11 21:22:51 -0600

    Refactored

--- First Unique Character in a String/unique.py
+++ First Unique Character in a String/unique.py
@@ -4,10 +4,7 @@ def firstUniqChar(s: str) -> int:
     d = {}
 
     for char in s:
-        if char in d:
-            d[char] += 1
-        else:
-            d[char] = 1
+        d[char] = d.get(char, 0) + 1
     
     for i in range(len(s)):
         if d[s[i]] == 1:
