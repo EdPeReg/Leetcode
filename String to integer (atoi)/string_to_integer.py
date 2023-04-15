ec2e192d3ec68bc7cd6a19df90952479734357ce
EdPeReg <dardo1818@gmali.com>
2023-04-15 11:05:32 -0600

    O(n) refactored and ChatGPT version added

--- String to integer (atoi)/string_to_integer.py
+++ String to integer (atoi)/string_to_integer.py
@@ -1,33 +1,60 @@
 def myAtoi(s: str) -> int:
-    if len(s) == 0 or len(s) == 1 and not s.isdigit():
+    if not s:
         return 0
 
     i32_lower = -(2**31)
     i32_upper = 2**31 - 1
-    result = "0"
+    result = 0
+    sign = 1
 
     # Remove left white spaces
     s = s.lstrip()
 
     for i, char in enumerate(s):
         # To know if it's negative
-        if i == 0 and s[i] == '-':
-            # We have an non digit next
-            if len(s) != 1 and not s[i+1].isdigit():
-                return 0
-            result = ""
-        elif i == 0 and s[i] == '+':
+        if i == 0 and char in ('-', '+'):
+            if char == '-':
+                sign = -1
             continue
-        elif not char.isdigit():
-            result = int(result)
+        if char.isdigit():
+            result = result * 10 + int(char)
+        else:
             break
-        # Append a digit
-        result += char
-    
-    result = int(result)
+
+    result *= sign
+    return min(max(result, i32_lower), i32_upper)
+
+# ChatGPT
+def myAtoiChatGPT(s: str) -> int:
+    if not s:
+        return 0
+
+    i32_lower = -(2**31)
+    i32_upper = 2**31 - 1
+    result = 0
+    sign = 1
+    i = 0
+
+    # Remove leading whitespaces
+    while i < len(s) and s[i] == ' ':
+        i += 1
+
+    # Check for optional sign
+    if i < len(s) and (s[i] == '+' or s[i] == '-'):
+        if s[i] == '-':
+            sign = -1
+        i += 1
+
+    # Process the remaining characters
+    while i < len(s) and s[i].isdigit():
+        result = result * 10 + int(s[i])
+        i += 1
+
+    result *= sign
 
     if result < i32_lower:
-        result = i32_lower
+        return i32_lower
     elif result > i32_upper:
-        result = i32_upper
-    return result
+        return i32_upper
+    else:
+        return result
