f96779a6f6efab65cae9903829ffcf47afc9e0f7
EdPeReg <dardo1818@gmali.com>
2024-04-07 13:17:48 -0600

    Adding more solutions

--- Merge Strings Alternately/main.py
+++ Merge Strings Alternately/main.py
@@ -1,14 +1,53 @@
 
+"""
+Leetcode problem: https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
+"""
+
+from itertools import zip_longest
+
 def mergeAlternately(word1: str, word2: str) -> str:
-    max_length = len(word1) if len(word1) > len(word2) else len(word2)
-    j = 0
+    """
+    Using two pointer approach, original solution.
+    
+    Complexity is 0(n+m) where n is the word1 length and m is word2 length.
+    """
+
+    w1_len, w2_len = len(word1), len(word2)
+    merge_str = ""
+    i, j = 0, 0
+
+    # Iterate over the words until both words
+    while i < w1_len or j < w2_len:
+        # Only add the char when the pointer is valid
+        if i < w1_len:
+            merge_str += word1[i]
+            i += 1
+        if j < w2_len:
+            merge_str += word2[j]
+            j += 1
+
+    return merge_str
+
+def mergeAlternatelyOnePointer(word1: str, word2: str) -> str:
+    """
+    One pointer approach.
+
+    Complexity is O(n+m) where n is word1 length and m is the word2 length
+    """
+
+    max_length = max(len(word1), len(word2))
     merge_str = ""
 
     for i in range(max_length):
         if i < len(word1):
             merge_str += word1[i]
-        if j < len(word2):
-            merge_str += word2[j]
-        j += 1
-    
+        if i < len(word2):
+            merge_str += word2[i]
+            
     return merge_str
+
+def mergeAlternatelyWeird(word1: str, word2: str) -> str:
+    """
+    Another solution from here https://leetcode.com/problems/merge-strings-alternately/editorial/comments/1867095
+    """
+    return ''.join(c for pair in zip_longest(word1, word2, fillvalue='') for c in pair)
--- Merge Strings Alternately/test.py
+++ Merge Strings Alternately/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import mergeAlternately
+from main import mergeAlternately, mergeAlternatelyOnePointer, mergeAlternatelyWeird
 
 class TestMergeAlternately(unittest.TestCase):
     def test01(self):
@@ -17,6 +17,53 @@ class TestMergeAlternately(unittest.TestCase):
         word1 = "abcd"
         word2 = "pq"
         self.assertEqual("apbqcd", mergeAlternately(word1, word2))
+
+    def test04(self):
+        word1 = "a"
+        word2 = "p"
+        self.assertEqual("ap", mergeAlternately(word1, word2))
+
+class TestMergeAlternatelyOnePointer(unittest.TestCase):
+    def test01(self):
+        word1 = "abc"
+        word2 = "pqr"
+        self.assertEqual("apbqcr", mergeAlternatelyOnePointer(word1, word2))
+
+    def test02(self):
+        word1 = "ab"
+        word2 = "pqrs"
+        self.assertEqual("apbqrs", mergeAlternatelyOnePointer(word1, word2))
+
+    def test03(self):
+        word1 = "abcd"
+        word2 = "pq"
+        self.assertEqual("apbqcd", mergeAlternatelyOnePointer(word1, word2))
+
+    def test04(self):
+        word1 = "a"
+        word2 = "p"
+        self.assertEqual("ap", mergeAlternately(word1, word2))
+
+class TestMergeAlternatelyWeird(unittest.TestCase):
+    def test01(self):
+        word1 = "abc"
+        word2 = "pqr"
+        self.assertEqual("apbqcr", mergeAlternatelyWeird(word1, word2))
+
+    def test02(self):
+        word1 = "ab"
+        word2 = "pqrs"
+        self.assertEqual("apbqrs", mergeAlternatelyWeird(word1, word2))
+
+    def test03(self):
+        word1 = "abcd"
+        word2 = "pq"
+        self.assertEqual("apbqcd", mergeAlternatelyWeird(word1, word2))
+
+    def test04(self):
+        word1 = "a"
+        word2 = "p"
+        self.assertEqual("ap", mergeAlternately(word1, word2))
 
 if __name__ == '__main__':
     unittest.main()
