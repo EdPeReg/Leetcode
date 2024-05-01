9943bd42218a3dadf2f9497e0fadbb734ef0c428
EdPeReg <dardo1818@gmali.com>
2024-04-30 20:18:48 -0600

    O(n) adding leetcode solution

--- Greatest Common Divisor of Strings/main.py
+++ Greatest Common Divisor of Strings/main.py
@@ -2,6 +2,7 @@
 """
 - Leetcode problem: https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
 - Method 2 to apply GCD: https://www.dcode.fr/gcd
+- Leetcode solution: https://leetcode.com/problems/greatest-common-divisor-of-strings/solutions/3124997/super-easy-solution-fully-explained-c-python3-java/
 """
 
 import math
@@ -28,3 +29,8 @@ def gcdOfStrings(str1: str, str2: str) -> str:
         j += 1
 
     return gcd_str
+
+def gcdOfStringsLeetcode(str1: str, str2: str) -> str:
+    if str1 + str2 == str2 + str1:
+        return str1[:math.gcd(len(str1), len(str2))]
+    return ""
--- Greatest Common Divisor of Strings/test.py
+++ Greatest Common Divisor of Strings/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import gcdOfStrings
+from main import gcdOfStrings, gcdOfStringsLeetcode
 
 class TestGCDofStrings(unittest.TestCase):
     def test_gcd(self):
@@ -16,6 +16,21 @@ class TestGCDofStrings(unittest.TestCase):
         in str1 so there is no gcd in this case. there can be no 'leftover'
         letters when finding a gcd."""
         self.assertEqual("", gcdOfStrings("ABCDEF", "ABC"))
+
+class TestGCDofStringsLeetcode(unittest.TestCase):
+    def test_gcd(self):
+        self.assertEqual("ABC", gcdOfStringsLeetcode("ABCABC", "ABC"))
+        self.assertEqual("AB", gcdOfStringsLeetcode("ABABAB", "ABAB"))
+        self.assertEqual("TAUXX", gcdOfStringsLeetcode("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"))
+        self.assertEqual("ABAB", gcdOfStringsLeetcode("ABABABAB", "ABAB"))
+        self.assertEqual("", gcdOfStringsLeetcode("ABABCCABAB", "ABAB"))
+    def test_gcd_empty(self):
+        self.assertEqual("", gcdOfStringsLeetcode("LEET", "CODE"))
+        """the expected output is correct because even though "ABC"
+        is found in str1 and str2, you have "DEF" that is left-over
+        in str1 so there is no gcd in this case. there can be no 'leftover'
+        letters when finding a gcd."""
+        self.assertEqual("", gcdOfStringsLeetcode("ABCDEF", "ABC"))
 
 if __name__ == '__main__':
     unittest.main()
